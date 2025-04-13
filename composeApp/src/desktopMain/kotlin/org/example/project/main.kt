468d211b22db8cc36e655738e75625cd5115109c
jkbehnam <jkbehnam@gmail.com>
2025-04-13 21:27:54 +0330

    first run

--- composeApp/src/desktopMain/kotlin/org/example/project/main.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/main.kt
@@ -2,12 +2,26 @@ package org.example.project
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import io.ktor.client.engine.cio.CIO
+import org.example.project.di.appModule
+import org.koin.core.context.GlobalContext.startKoin
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "MovieListKmp",
-    ) {
-        App()
+fun main() {
+    // Create an HTTP engine instance
+    val httpEngine = CIO.create()  // You can replace CIO with another engine like OkHttp, Apache, etc.
+
+    // Initialize Koin before starting the application
+    startKoin {
+        modules(appModule(httpEngine))
+    }
+
+    // Then start your application
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "MovieListKmp",
+        ) {
+            App()
+        }
     }
 }
--- composeApp/src/wasmJsMain/kotlin/org/example/project/main.kt
+++ composeApp/src/wasmJsMain/kotlin/org/example/project/main.kt
@@ -1,12 +1,13 @@
 package org.example.project
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import org.jetbrains.compose.web.renderComposable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import kotlinx.browser.document
 
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     initKoin()
-    renderComposable(rootElementId = "root") {
+    ComposeViewport(document.body!!) {
         App()
     }
 }
