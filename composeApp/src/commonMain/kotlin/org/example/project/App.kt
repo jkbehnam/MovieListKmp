e90ef670f994551cbcb59ce43839b9a2a5852adc
B.beigzadeh <jkbehnam@gmail.com>
2025-04-14 14:56:48 +0330

    save

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -5,12 +5,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.navigation.compose.rememberNavController
 import org.example.project.navigation.AppNavigation
 import org.example.project.ui.component.MainTopAppBar
@@ -19,33 +22,32 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
-fun App(
-
-) {
-
+fun App() {
     var darkTheme by remember { mutableStateOf(false) }
     val navController = rememberNavController()
-
     val backStack by navController.currentBackStack.collectAsState()
 
-    Theme (darkTheme = darkTheme) {
-        Scaffold(topBar = {
-            MainTopAppBar(
-                appName = "TMDB List",
-                darkTheme = darkTheme,
-                changeTheme = { darkTheme = !darkTheme },
-                showBackButton= backStack.size > 2,
-                goBack = {
-                    navController.popBackStack()
+    // Force LTR layout
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+        Theme(darkTheme = darkTheme) {
+            Scaffold(topBar = {
+                MainTopAppBar(
+                    appName = "TMDB List",
+                    darkTheme = darkTheme,
+                    changeTheme = { darkTheme = !darkTheme },
+                    showBackButton = backStack.size > 2,
+                    goBack = {
+                        navController.popBackStack()
+                    }
+                )
+            }) { padding ->
+                Box(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(padding)
+                ) {
+                    AppNavigation(navController)
                 }
-            )
-        }) { padding ->
-            Box(
-                Modifier
-                    .fillMaxSize()
-                    .padding(padding)
-            ) {
-                AppNavigation(navController)
             }
         }
     }
